ac5fe9d23dc1e623a616f8f3b4f0f4bc1e828fd7
Masaki Furuta <masaki.furuta@gmail.com>
2020-03-19 16:23:32 +0900

    Make resources configurable.

--- 07_install_bootstrap.sh
+++ 07_install_bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ./setup.conf
+
 ROLE=bootstrap
 cp -p boot.ipxe.${ROLE} /usr/share/nginx/html/ipxe/boot.ipxe
 
@@ -12,9 +14,9 @@ virt-install \
       --os-type linux \
       --os-variant rhel8.0 \
       --network network=ocp,mac="52:54:00:00:01:01" \
-      --vcpus 4 \
-      --ram 16384 \
-      --disk pool=default,size=100,format=qcow2 \
+      --vcpus ${BCPU} \
+      --ram ${BRAM} \
+      --disk pool=default,size=${BDISK},format=qcow2 \
       --check disk_size=off \
       --nographics \
       --noautoconsole \
--- 08_install_master.sh
+++ 08_install_master.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+. ./setup.conf
+
 ROLE=master
 cp -p boot.ipxe.${ROLE} /usr/share/nginx/html/ipxe/boot.ipxe
 
-for i in `seq 0 2`; do
+for i in `seq 0 $((${MASTERS}-1))`; do
     mac=$((i+2))
     virt-install \
       --name master-$i \
@@ -14,9 +16,9 @@ for i in `seq 0 2`; do
       --os-type linux \
       --os-variant rhel8.0 \
       --network network=ocp,mac="52:54:00:00:01:0${mac}" \
-      --vcpus 8 \
-      --ram 16384 \
-      --disk pool=default,size=100,format=qcow2 \
+      --vcpus ${MCPU} \
+      --ram ${MRAM} \
+      --disk pool=default,size=${MDISK},format=qcow2 \
       --check disk_size=off \
       --nographics \
       --noautoconsole \
