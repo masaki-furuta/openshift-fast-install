bdef7e44875207a1cb6b61d71699cd5941a62742
Masaki Furuta <masaki.furuta@gmail.com>
2020-07-24 06:43:42 +0900

    Suppress error.

--- approve_csr.sh
+++ approve_csr.sh
@@ -2,5 +2,5 @@
 #set -xv
 
 export KUBECONFIG=$(find . -name kubeconfig | fgrep /bare-metal/)
-oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve
+oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve 2>/dev/null && \
 oc get nodes
