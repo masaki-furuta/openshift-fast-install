32bf8d5c9f123d502de8e6ecb640814b09de7d72
Masaki Furuta <masaki.furuta@gmail.com>
2020-07-24 06:42:26 +0900

    Adjust install order for 4.5+.

--- 00_all.sh
+++ 00_all.sh
@@ -3,6 +3,8 @@
 export LC_ALL=C
 export LANG=C
 
+. ./setup.conf
+
 testenv() {
     ret=$(getenforce | grep -q Permissive ; echo $?)
     if [[ $ret -ne 0 ]];then
@@ -15,30 +17,58 @@ testenv() {
 }
 
 lists() {
-cat <<EOF
-### 初期設定値を反映させる
-mk_ocp.xml.sh
-mk_Corefile.sh
-mk_db.NETWORK_1.sh
-mk_db.lab.local.sh
-mk_boot.ipxe.sh
-### 既存のVM環境を削除
-destroy_env.sh
-### 設定開始
-01_setup_libvirtd.sh
-02_setup_firewalld.sh
-03_install_coredns.sh
-04_install_nginx.sh
-05_setup_ipxe.sh
-06_setup_openshift_install-config.sh
-07_install_bootstrap.sh
-08_install_master.sh
-09_install_worker.sh
-10_setup_oc_command.sh
-## checking
-11_confirm.sh
-12_check.sh
-EOF
+    if [[ $(echo "${VERSION%.*} >= 4.5" | bc -l) ]]; then
+	cat <<- EOF
+		### Reflects the default settings.
+		mk_ocp.xml.sh
+		mk_Corefile.sh
+		mk_db.NETWORK_1.sh
+		mk_db.lab.local.sh
+		mk_boot.ipxe.sh
+		### Remove existing VM environment.
+		destroy_env.sh
+		### Start configuration.
+		01_setup_libvirtd.sh
+		02_setup_firewalld.sh
+		03_install_coredns.sh
+		04_install_nginx.sh
+		05_setup_ipxe.sh
+		06_setup_openshift_install-config.sh
+		07_install_bootstrap.sh
+		08_install_master.sh
+		10_setup_oc_command.sh
+		## checking
+		11_confirm.sh
+		09_install_worker.sh
+		12_check.sh
+		EOF
+    else
+	cat <<- EOF
+		### Reflects the default settings.
+		mk_ocp.xml.sh
+		mk_Corefile.sh
+		mk_db.NETWORK_1.sh
+		mk_db.lab.local.sh
+		mk_boot.ipxe.sh
+		### Remove existing VM environment.
+		destroy_env.sh
+		### Start configuration.
+		01_setup_libvirtd.sh
+		02_setup_firewalld.sh
+		03_install_coredns.sh
+		04_install_nginx.sh
+		05_setup_ipxe.sh
+		06_setup_openshift_install-config.sh
+		07_install_bootstrap.sh
+		08_install_master.sh
+		09_install_worker.sh
+		10_setup_oc_command.sh
+		## checking
+		11_confirm.sh
+		12_check.sh
+		EOF
+    fi
+
 }
 
 testenv
