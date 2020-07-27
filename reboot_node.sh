4ec2a252421f48474039949279dcfa542a683f36
Masaki Furuta <masaki.furuta@gmail.com>
2020-07-28 08:03:48 +0900

    Ask private key to run reboot_node.sh
    Signed-off-by: Masaki Furuta <masaki.furuta@gmail.com>

--- reboot_node.sh
+++ reboot_node.sh
@@ -2,12 +2,19 @@
 
 #set -xv
 
+. ./setup.conf
+
 main () {
+    selectKey "KEY" "Where's your ssh private key file ?"
     selectNode "NODE" "Which node to reboot ?"
     echo "Run ssh -i ~/.ssh/id_rsa core@${NODE} sudo reboot .."
-    ssh -i ~/.ssh/id_rsa core@${NODE} sudo reboot
+    ssh -i ${KEY} core@${NODE} sudo reboot
 }
-    
+
+selectKey () {
+    read -p "${2} `echo $'\n: '`" ${1}
+    export ${1}=${!1}
+}
 
 selectNode () {
     read -p "${2} `echo $'\n: '`" ${1}
