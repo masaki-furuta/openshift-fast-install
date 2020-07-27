f69a94926703ab37088424690a947d020f69db2f
Masaki Furuta <masaki.furuta@gmail.com>
2020-07-28 05:06:01 +0900

    Minor fix.
    Signed-off-by: Masaki Furuta <masaki.furuta@gmail.com>

--- 11_confirm.sh
+++ 11_confirm.sh
@@ -5,8 +5,8 @@ echo "If the following commands do not complete in more than 30 minutes, it is l
 echo "To watch its status, run e.g."
 echo "ssh -i .ssh/id_rsa core@bootstrap journalctl -f"
 echo "ssh -i .ssh/id_rsa core@master-0 journalctl -f"
-echo "                     :"                 
-echo "ssh -i .ssh/id_rsa core@worker-0 journalctl -f"
+#echo "                     :"                 
+#echo "ssh -i .ssh/id_rsa core@worker-0 journalctl -f"
 echo "                     :"                 
 
 if [[ x$DEBUG_INSTALL == xY ]]; then
