feabf28b2019adbc4cc4af7cb4aa1acc634cd835
Masaki Furuta <masaki.furuta@gmail.com>
2020-07-31 12:58:59 +0900

    Keep port 80 for nginx.

--- 04_install_nginx.sh
+++ 04_install_nginx.sh
@@ -10,14 +10,16 @@ notFound() {
 
 notFound nginx nginx-mod-stream
 
-mkdir -p /usr/share/nginx/html/ipxe/
-mkdir -p /usr/share/nginx/html/ocp/rhcos/ignitions
-mkdir -p /usr/share/nginx/html/ocp/rhcos/images/latest
-chmod -R 755 /usr/share/nginx/html/
+sudo mkdir -p /usr/share/nginx/html/ipxe/
+sudo mkdir -p /usr/share/nginx/html/ocp/rhcos/ignitions
+sudo mkdir -p /usr/share/nginx/html/ocp/rhcos/images/latest
+sudo chmod -R 755 /usr/share/nginx/html/
 
-cp -p ./etc_conf/nginx/nginx.conf /etc/nginx/nginx.conf
-systemctl restart nginx
+sudo cp -p ./etc_conf/nginx/nginx.conf /etc/nginx/nginx.conf
+echo "Stopping HTTPd for preserving port 80"
+sudo systemctl stop httpd
+sudo systemctl restart nginx
 
 sleep 5
 
-systemctl show -p SubState --value nginx || exit 1
+sudo systemctl show -p SubState --value nginx || exit 1
